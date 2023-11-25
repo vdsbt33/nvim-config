d55bef3cf649a0e3b7047dd5e3b3e90146ba5e9b
Vinicius de Souza <vdsbt33@gmail.com>
2023-11-24 22:33:46 -0300

    Fixed lua server and README

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'eslint', 'gopls', 'lua-language-server'},
+  ensure_installed = {'tsserver', 'rust_analyzer', 'eslint', 'gopls', 'lua_ls'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
